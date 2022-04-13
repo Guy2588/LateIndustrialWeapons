74f0204227fcd4fca84ad1507121f703de8d3499
Guy2588 <101416040+Guy2588@users.noreply.github.com>
2022-04-13 16:43:47 -0400

    Add files via upload

---
+++ LateIndustrialWeapons/CE/Source/LateIndustrialWeapons/CompProperties_AmmoUser_WithAltGunshot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using CombatExtended;
+
+namespace LateIndustrialWeapons
+{
+    public class CompProperties_AmmoUser_WithAltGunshot : CompProperties_AmmoUser
+    {
+        public SoundDef normalSound;
+        public SoundDef alternateSound;
+        public List<AmmoDef> ammoDefsForAltSound;
+
+        public CompProperties_AmmoUser_WithAltGunshot()
+        {
+            compClass = typeof(CompAmmoUser_WithAltGunshot);
+        }
+    }
+}
